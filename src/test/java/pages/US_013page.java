e53c3ac1c89dfa498a4009eb0a516556e61d5cc3
Ustaoglu <erenustaoglu98@gmail.com>
2022-06-21 17:13:27 +0300

    US_013 olusturuldu

--- src/test/java/pages/US_013page.java
+++ src/test/java/pages/US_013page.java
@@ -1,4 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_013page {
+
+    public US_013page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "")
+    public WebElement deneme;
+
 }
--- src/test/java/tests/US_013test.java
+++ src/test/java/tests/US_013test.java
@@ -1,4 +1,12 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class US_013test {
+
+
+    @Test
+    public void TC_001() {
+        System.out.println("deneme");
+    }
 }
