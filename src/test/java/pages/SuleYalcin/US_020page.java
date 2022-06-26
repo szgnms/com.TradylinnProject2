89506b63e55e0194e4e4afc0bd79ef2de2d45930
Sule YALCIN <suleyalcin983@gmail.com>
2022-06-26 23:14:31 +0300

    26 haziran sule

--- src/test/java/pages/SuleYalcin/US_020page.java
+++ src/test/java/pages/SuleYalcin/US_020page.java
@@ -1,7 +1,22 @@
 package pages.SuleYalcin;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class US_020page {
 
 
+    @FindBy(xpath = "//body/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[17]/a[1]/span[2]")
+    public static WebElement incelemeler;
+
+    @FindBy(xpath = "//thead//tr//th[3]")
+    public static WebElement author;
+
+    @FindBy(xpath = "//thead//tr//th[4]")
+    public static WebElement comment;
+    @FindBy(xpath = "//thead//tr//th[5]")
+    public static WebElement rating;
+    @FindBy(xpath = "//thead//tr//th[6]")
+    public static WebElement dated;
 
 }
