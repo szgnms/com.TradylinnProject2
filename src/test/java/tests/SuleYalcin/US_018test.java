eaf66e08b2d7fff9c8d03443f478abcfc0f237b2
SZGN <szgnmhmmd@gmail.com>
2022-06-29 14:22:43 +0300

    29/06

--- src/test/java/tests/SuleYalcin/US_018test.java
+++ src/test/java/tests/SuleYalcin/US_018test.java
@@ -1,9 +1,6 @@
 package tests.SuleYalcin;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,13 +12,15 @@ import utilities.Driver;
 import utilities.ReusableMethods;
 import utilities.TestBaseRapor;
 
+import java.util.ArrayList;
+
 import static org.openqa.selenium.Keys.PAGE_DOWN;
 import static pages.SuleYalcin.US_018page.*;
 import static utilities.ReusableMethods.bekle;
 import static utilities.ReusableMethods.hover;
 
 public class US_018test extends TestBaseRapor{
-
+Actions act = new Actions(Driver.getDriver());
     public US_018test() {
 
     }
@@ -31,15 +30,63 @@ public class US_018test extends TestBaseRapor{
         ReusableMethods.tradylinnStoreManager();
         bekle();
         Thread.sleep(10000);
-        US_018page.emirlerLinki.click();
+        act.sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
         bekle();
-        refundRequestReason.click();
         bekle();
-        hover(qty);
+        bekle();
+        bekle();
+        bekle();
+        bekle();
+        bekle();
+        bekle();
+        bekle();
+        bekle();
+        ArrayList <WebElement> refreq =new ArrayList<>(Driver.getDriver().findElements(By.xpath("(//*[@id='wcfm-orders']//td)//a")));
+       if (refreq.get(3).isDisplayed()){
+           refreq.get(3).click();
+            Driver.getDriver().switchTo().alert().accept();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+            bekle();
+        }
 
-        US_018page.qty.sendKeys("1");
-        refundRequestReason.sendKeys("Begenmedim");
-        submit.click();
+        refreq.get(3).click();
+        bekle();
+        bekle();
+        ArrayList <WebElement> qty =new ArrayList<>(Driver.getDriver().findElements(By.xpath("//tbody//tr[1]//td[3]//select[1]//option")));
+        qty.get(1).click();
+        act.sendKeys(Keys.TAB).
+        sendKeys(Keys.TAB)
+       .sendKeys("Begenmedim");
+        bekle();
+        bekle();
+        act.sendKeys(Keys.TAB).sendKeys(Keys.SPACE).perform();
 
 /*
         JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
@@ -51,19 +98,43 @@ public class US_018test extends TestBaseRapor{
         refundRequestReason.sendKeys("Begenmedim");
         US_018page.submit.click();
         bekle();  */
-
-        hover(geriOdemeButonu);
-
-        geriOdemeButonu.click();
         bekle();
-        hover(requestId);
-
+        bekle();
+        bekle();
+        bekle();
+        bekle();
+        act.sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        bekle();
+        bekle();
+        bekle();
+        ArrayList<WebElement>refRegTable = new ArrayList<>(Driver.getDriver().findElements(By.xpath("(//*[@id='wcfm-refund-requests']//tr)[1]//th")));
         SoftAssert sf=new SoftAssert();
+        for (WebElement w:refRegTable
+             ) {
+            sf.assertTrue(w.isDisplayed());
+        }
 
-        sf.assertTrue(US_018page.orderId.isDisplayed());
-        sf.assertTrue(US_018page.amount.isDisplayed());
-        sf.assertTrue(US_018page.type.isDisplayed());
-        sf.assertTrue(US_018page.reasonText.isDisplayed());
         sf.assertAll();
 
 
