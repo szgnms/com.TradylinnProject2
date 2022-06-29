32129f36f8beb9ae80249ec2d87293b967721170
Lenovo <seckinkaraca4@gimail.com>
2022-06-29 04:32:14 +0300

    tamam

--- src/test/java/pages/SeckinKaraca/US_22page.java
+++ src/test/java/pages/SeckinKaraca/US_22page.java
@@ -13,5 +13,13 @@ public class US_22page {
     @FindBy(xpath = "//*[@id=\"main\"]/div/div/div[1]/div/div/div/section/div/div[2]/div/div/div/h2/a")
     public WebElement tümünügör;
     @FindBy(xpath = "//select[@class='orderby form-control']")
-    public WebElement varsayılanSıralama;
+    public WebElement siralama;
+    @FindBy(xpath = "//span[@class='woocommerce-Price-amount amount']")
+    public WebElement fiyatlar;
+    @FindBy(xpath = "(//h3[@class='woocommerce-loop-product__title'])[1]")
+    public WebElement enUcuz;
+    @FindBy(xpath = "(//span[@class='woocommerce-Price-amount amount'])[18]")
+    public WebElement onİkinci;
+    @FindBy(xpath = "(//span[@class='woocommerce-Price-amount amount'])[1]")
+    public WebElement enPahali;
     }
